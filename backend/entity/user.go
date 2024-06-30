594bd4dc5ef8130c1decc747093e3ae643cefcf9
drakoryufp <165974253+drakoryufp@users.noreply.github.com>
2024-06-30 02:30:56 +0100

    add 1.user registration

--- backend/entity/user.go
+++ backend/entity/user.go
@@ -1,5 +1,9 @@
 package entity
 
+import (
+	"time"
+)
+
 type User struct {
 	ID             uint64 `gorm:"primary_key:auto_increment" json:"id"`
 	Name           string `gorm:"type:varchar(255)" json:"name"`
@@ -7,4 +11,6 @@ type User struct {
 	Email          string `gorm:"uniqueIndex;type:varchar(255)" json:"email"`
 	ProfilePicture string `gorm:"type:varchar(1024);default:'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=170667a&w=0&h=kEAA35Eaz8k8A3qAGkuY8OZxpfvn9653gDjQwDHZGPE='"  json:"profile_picture"`
 	Token          string `gorm:"-" json:"token"`
+	CreatedAt 	   time.Time `gorm:"autoCreateTime" json:"created_at"`
+    UpdatedAt      time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
