c599b411db222eb33ac4663d69bd4b5eac738399
drakoryufp <165974253+drakoryufp@users.noreply.github.com>
2024-07-08 02:57:13 +0100

    reparar erro jwt_service

--- backend/service/user_service.go
+++ backend/service/user_service.go
@@ -12,7 +12,7 @@ import (
 
 func Register(userDTO dto.RegisterDTO) {//dto.UserIDResponseDTO {
 	user := entity.User{}
-	userResponse := dto.UserIDResponseDTO{}
+	userResponse := dto.UserResponseDTO{}
 
 	err := smapping.FillStruct(&user, smapping.MapFields(&userDTO))
 	if err != nil {
@@ -30,9 +30,9 @@ func Register(userDTO dto.RegisterDTO) {//dto.UserIDResponseDTO {
 	//return userResponse
 }
 
-func Profile(id uint64) (dto.UserIDResponseDTO,error) {
+func Profile(id uint64) (dto.UserResponseDTO,error) {
 	
-	userResponse := dto.UserIDResponseDTO{}
+	userResponse := dto.UserResponseDTO{}
 
 	user, err := repository.GetUser(id)
 	if err != nil {
@@ -48,12 +48,26 @@ func Profile(id uint64) (dto.UserIDResponseDTO,error) {
 	return userResponse, nil
 }
 
-func UpdateProfile(user entity.User, id uint64) error {
-	user.ID=id
-	if err := repository.UpdateUser(user); err == nil{
-		return nil 
+func UpdateProfile(userDTO dto.UserUpdateDTO, id uint64) (dto.UserResponseDTO,error) {
+	var user entity.User
+	var userResponse dto.UserResponseDTO
+	
+	err := smapping.FillStruct(&user, smapping.MapFields(&userDTO))
+	if err != nil {
+		log.Fatal("failed to map to response ", err)
+		return userResponse, err
 	}
-	return errors.New("user do not exist")
+
+	user.ID = id
+	user = repository.UpdateUser(user);
+
+	err = smapping.FillStruct(&userResponse, smapping.MapFields(&user))
+	if err != nil {
+		log.Fatal("failed to map to response ", err)
+		return userResponse, err
+	}
+
+	return userResponse, nil
 }
 
 func DeleteAccount(identifiant uint64) error {
